d59d987bf010e7ce9f83188a48533e04f0885664
7-Qwen <dedsec.7wen@gmail.com>
2024-02-27 17:04:32 +0800

    init

---
+++ src/main/java/com/example/learn_to_all_spring/LearnToAllSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.learn_to_all_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnToAllSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnToAllSpringApplication.class, args);
+    }
+
+}
